1b1d91d98dcf221a8970c841e2f1b2f01fe1cdcf
miggo <miggo@pop-os.localdomain>
2023-05-02 19:49:15 +0300

    renamed base folder, cleaned some unused files

--- backend/apps.py
+++
@@ -1,4 +0,0 @@
-INSTALLED_APPS = [
-    webchat,
-    webchatapp,
-]
--- backend/src/webchat/settings.py
+++ backend/src/webchat/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'webchatapp.apps.WebchatappConfig',
+    'webchatapp',
 ]
 
 MIDDLEWARE = [
